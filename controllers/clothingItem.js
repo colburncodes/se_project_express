1e2aa8dc9c9df32c58c2327648325d9a9217095b
colburncodes <colburnsanders@gmail.com>
2023-02-20 21:46:48 -0600

    refactor

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-console */
 const ClothingItem = require("../models/clothingItem");
 
-const getItems = (req, res) => {
+const getItems = (req, res, next) => {
   ClothingItem.find({})
     .orFail()
     .then((items) => res.status(200).send({ data: items }))
-    .catch((error) =>
-      res.status(500).send({ message: "Error making request", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const findById = (req, res) => {
+const findById = (req, res, next) => {
   const { id } = req.params;
 
   ClothingItem.findById(id)
     .orFail()
     .then((item) => res.status(200).send(item))
-    .catch((error) =>
-      res.status(500).send({ message: "No clothing item found.", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const createItem = (req, res) => {
+const createItem = (req, res, next) => {
   console.log(req.user._id);
 
   const { name, weather, imageUrl } = req.body;
@@ -31,35 +31,34 @@ const createItem = (req, res) => {
       res.status(200).send({ data: item });
     })
     .catch((error) => {
-      res.status(500).send({ message: "Error creating item", error });
+      next(error);
     });
 };
 
-const updateItem = (req, res) => {
+const updateItem = (req, res, next) => {
   const { id } = req.params;
   const { name, weather, imageUrl } = req.body;
 
   ClothingItem.findByIdAndUpdate(id, { $set: { name, weather, imageUrl } })
     .orFail()
     .then((item) => res.status(200).send({ item }))
-    .catch((error) =>
-      res.status(400).send({ message: "Error updating item", error })
-    )
-    .catch((error) => res.status(500).send({ message: "Server Error", error }));
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const deleteItem = (req, res) => {
+const deleteItem = (req, res, next) => {
   const { id } = req.params;
 
   ClothingItem.findByIdAndDelete(id)
     .orFail()
     .then((item) => res.status(204).send(item))
-    .catch((error) =>
-      res.status(500).send({ message: "Error Item Not Found", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const likeItem = (req, res) => {
+const likeItem = (req, res, next) => {
   const { id } = req.params;
 
   ClothingItem.findByIdAndUpdate(
@@ -68,12 +67,12 @@ const likeItem = (req, res) => {
     { new: true }
   )
     .then((item) => res.status(200).send(item))
-    .catch((error) =>
-      res.status(500).send({ message: "Internal Server Error", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const dislikeItem = (req, res) => {
+const dislikeItem = (req, res, next) => {
   const { id } = req.params;
 
   ClothingItem.findByIdAndUpdate(
@@ -82,9 +81,9 @@ const dislikeItem = (req, res) => {
     { new: true }
   )
     .then((item) => res.status(200).send(item))
-    .catch((error) =>
-      res.status(500).send({ message: "Internal Server Error", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
 module.exports = {
--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,28 @@
 const User = require("../models/user");
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .orFail()
     .then((user) => {
       res.status(200).send({ data: user });
     })
-    .catch((error) =>
-      res.status(500).send({ message: "Error making request", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const getUserById = (req, res) => {
+const getUserById = (req, res, next) => {
   const { userId } = req.params;
 
   User.findById(userId)
     .orFail()
     .then((user) => res.status(200).send(user))
-    .catch((error) =>
-      res.status(404).send({ message: "User does not exist", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
@@ -30,21 +30,21 @@ const createUser = (req, res) => {
     .then((user) => {
       res.status(200).send({ data: user });
     })
-    .catch(() =>
-      res.status(500).send({ message: "Error creating user", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const updateUser = (req, res) => {
+const updateUser = (req, res, next) => {
   const { userId } = req.params;
   const { name, avatar, about } = req.body;
 
   User.findByIdAndUpdate(userId, { $set: { name, avatar, about } })
     .orFail()
     .then((user) => res.status(200).send({ data: user }))
-    .catch((error) =>
-      res.status(500).send({ message: "Error updating user", error })
-    );
+    .catch((error) => {
+      next(error);
+    });
 };
 
 module.exports = {
